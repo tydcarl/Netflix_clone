616b6876164b95414e4f1bf013b0235c2db278fb
Tyler <tydcarl@gmail.com>
2026-03-02 13:45:03 -0800

    42:30

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import './Home.css'
-import Navbar from '../../components/Navbar/Navbar'
+import React from "react";
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import hero_title from "../../assets/hero_title.png";
 
 const Home = () => {
   return (
-    <div className='home'>
-        <Navbar/>
+    <div className="home">
+      <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner_img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>
+            Discovering his ties to a secret anicient order, a young man living
+            in modern Istanbul embarks on a quest to save the city from an
+            immortal enemy. 
+            <div className="hero-btns">
+                <button><img src="" alt="" />Play</button>
+            </div>
+          </p>
         </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
