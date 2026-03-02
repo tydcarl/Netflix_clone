c9755399d1a03a4931b2b6703b04345557b45e0f
Tyler <tydcarl@gmail.com>
2026-03-02 15:28:10 -0800

    54

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import './TitleCards.css'
+import React from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = () => {
   return (
-    <div className='titlecards'></div>
-  )
-}
+    <div className="title-cards">
+      <h2>Popular on Netlfix</h2>
+      <div className="card-list">
+        {cards_data.map((card, index) => {
+            return (
+              <div className="card" key={index}>
+                <img src={card.image} alt="" />
+                <p>{card.name}</p>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
 
-export default TitleCards
+export default TitleCards;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,9 @@ import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 import hero_banner from "../../assets/hero_banner.jpg";
 import hero_title from "../../assets/hero_title.png";
-import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
+import TitleCards from "../../components/TitleCards/TitleCards";
 
 const Home = () => {
   return (
@@ -18,8 +20,10 @@ const Home = () => {
             in modern Istanbul embarks on a quest to save the city from an
             immortal enemy. 
             <div className="hero-btns">
-                <button><img src="" alt="" />Play</button>
+                <button className='btn'><img src={play_icon} alt="" />Play</button>
+                <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
             </div>
+            <TitleCards/>
           </p>
         </div>
       </div>
