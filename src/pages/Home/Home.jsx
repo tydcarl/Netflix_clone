9f473b0c4ae01e335ea51d26f379f9ff3643b445
Tyler <tydcarl@gmail.com>
2026-03-05 15:38:19 -0800

    1:20

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,19 +20,19 @@ const Home = () => {
           <p>
             Discovering his ties to a secret anicient order, a young man living
             in modern Istanbul embarks on a quest to save the city from an
-            immortal enemy.
-            <div className="hero-btns">
-              <button className="btn">
-                <img src={play_icon} alt="" />
-                Play
-              </button>
-              <button className="btn dark-btn">
-                <img src={info_icon} alt="" />
-                More Info
-              </button>
-            </div>
-            <TitleCards />
+            immortal enemy.{" "}
           </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" />
+              More Info
+            </button>
+          </div>
+          <TitleCards />
         </div>
       </div>
       <div className="more-cards">
