1bdfa97bb1b487e8289939ca7967b0b9cabebe58
Tyler <tydcarl@gmail.com>
2026-03-17 19:18:46 -0700

    2:28

---
+++ src/firebase.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, EmailAuthProvider, getAuth } from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-CPpZK3H0OlRekn6XaykFsmVaxjfNE1g",
+  authDomain: "netflix-clone-38199.firebaseapp.com",
+  projectId: "netflix-clone-38199",
+  storageBucket: "netflix-clone-38199.firebasestorage.app",
+  messagingSenderId: "234016206134",
+  appId: "1:234016206134:web:69b5fed519f5d369039797",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password)=>{
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc(collection(db, "user"), {
+            uid: user.uid, 
+            name, 
+            EmailAuthProvider: "local", 
+            email,
+        })
+    } catch (error)  {
+        console.log(error);
+        alert(error);
+
+    }
+
+}
+
+
