7ccdf8bdf8932e7562c14e4dc07fae500b85d036
Tyler <tydcarl@gmail.com>
2026-03-19 11:48:54 -0700

    2:53

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./pages/Home/Home";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Player from "./pages/Player/Player";
 import { onAuthStateChanged } from "firebase/auth";
+import { ToastContainer, toast } from "react-toastify";
 import { auth } from "./firebase";
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
   }, []);
   return (
     <div>
+      <ToastContainer theme='dark' />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, 
-    EmailAuthProvider, 
-    getAuth, 
-    signInWithEmailAndPassword, 
-    signOut} from "firebase/auth";
-import { addDoc, 
-    collection, 
-    getFirestore } from "firebase/firestore";
+import {
+  createUserWithEmailAndPassword,
+  EmailAuthProvider,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB-CPpZK3H0OlRekn6XaykFsmVaxjfNE1g",
@@ -21,39 +22,33 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const signup = async (name, email, password)=>{
-    try {
-        const res = await createUserWithEmailAndPassword(auth, email, password);
-        const user = res.user;
-        await addDoc(collection(db, "user"), {
-            uid: user.uid, 
-            name, 
-            EmailAuthProvider: "local", 
-            email,
-        })
-    } catch (error)  {
-        console.log(error);
-        alert(error);
+const signup = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "user"), {
+      uid: user.uid,
+      name,
+      EmailAuthProvider: "local",
+      email,
+    });
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
 
-    }
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
 
-}
+const logout = () => {
+  signOut(auth);
+};
 
-const login = async(email, password)=> {
-    try {
-       await signInWithEmailAndPassword(auth, email, password) 
-    } catch (error) {
-        console.log(error);
-        alert(error);
-
-    }
-
-}
-
-const logout = ()=> {
-    signOut(auth);
-}
-
-export {auth, db, login, signup, logout}
-
-
+export { auth, db, login, signup, logout };
